d6fa5fe3682b5362db5195fffb0907077009714c
rumronya <rum.ronya@gmail.com>
2025-08-29 11:55:05 +0300

    added jwt 0.2

---
+++ src/main/java/org/innowise/internship/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.innowise.internship.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, ReactiveJwtDecoder jwtDecoder) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/v1/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtSpec -> jwtSpec
+                                .jwtDecoder(jwtDecoder) // явно указываем декодер JWT
+                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        // секрет для HMAC (из application.properties можно взять через @Value)
+        String secretKey = "my-secret-key";
+        return NimbusReactiveJwtDecoder.withSecretKey(
+                new SecretKeySpec(secretKey.getBytes(), "HMACSHA256")
+        ).build();
+    }
+
+}
